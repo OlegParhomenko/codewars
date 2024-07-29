8ff98333b9c9ff43a09068b1b281eda468e72999
Oleg Parhomenko <shocked194@gmail.com>
2024-07-29 21:25:40 +0300

    game move

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 1. Multiply
 2. Return Negative
 3. Convert boolean values to strings 'Yes' or 'No'.
+4. Grasshopper - Terminal game move function
 
---
+++ game move.js
@@ -0,0 +1,12 @@
+//  Insstruction - Terminal game move function 🤔🤔🤔
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Skills: -FUNDAMENTALS
+
+// Example: move(3, 6) should equal 15
+
+// Solution   ✔✔✔
+function move (position, roll) {
+	return position + roll * 2
+}
