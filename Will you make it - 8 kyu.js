e7a0e608cae23377b4331fbe0272eb6a1bdcd961
Oleg Parhomenko <shocked194@gmail.com>
2024-11-05 18:22:46 +0300

    Will you make it

---
+++ Will you make it - 8 kyu.js
@@ -0,0 +1,34 @@
+// Вы были в походе с друзьями далеко от дома, но когда пришло время возвращаться, вы поняли, что топливо заканчивается, а ближайшая заправка находится 50в нескольких милях! Вы знаете, что в среднем ваша машина проезжает около 25миль на галлон. Остались 2галлоны.
+
+// Учитывая эти факторы, напишите функцию, которая сообщит вам, возможно ли добраться до насоса или нет.
+
+// Функция должна возвращать значение, trueесли это возможно, и false наоборот.
+
+Skills: 
+
+// Решение с помощью if
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//     if((mpg * fuelLeft) >= distanceToPump){
+//         return true
+//     } else {
+//         return false
+//     }
+//   };
+
+  // Решение с помощью тернарного оператора
+
+//   const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//     return mpg * fuelLeft >= distanceToPump? true: false
+//   };
+
+    // Решение с помощью switch/case
+
+    // const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    //     switch(true){
+    //         case((mpg * fuelLeft) >= distanceToPump):
+    //             return true;
+    //         default:
+    //              return false
+    //     }
+    //   };
